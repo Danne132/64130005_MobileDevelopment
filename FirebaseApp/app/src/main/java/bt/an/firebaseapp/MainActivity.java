44d5c3913307a54d17796a61f3c9bfcbd4c409b2
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-12 14:54:13 +0700

    update lấy dữ liệu từ firebase

--- FirebaseApp/app/src/main/java/bt/an/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/bt/an/firebaseapp/MainActivity.java
@@ -1,6 +1,8 @@
 package bt.an.firebaseapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +35,24 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, Duy An");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+                Toast.makeText(getBaseContext(), "Value is: "+ value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+                Toast.makeText(getBaseContext(), "Failed to read value", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
