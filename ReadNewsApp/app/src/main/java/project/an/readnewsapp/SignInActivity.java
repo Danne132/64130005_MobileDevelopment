63b0f102293c7bb246336c3ed694ed8d08831b7e
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-17 00:07:05 +0700

    update chức năng đăng nhập

--- ReadNewsApp/app/src/main/java/project/an/readnewsapp/SignInActivity.java
+++ ReadNewsApp/app/src/main/java/project/an/readnewsapp/SignInActivity.java
@@ -1,19 +1,25 @@
 package project.an.readnewsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,7 +27,7 @@ public class SignInActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     TextInputEditText inputEmailSignIn, inputPassSignIn;
-    TextView forgetPass;
+    TextView forgetPass, needPass, needMail;
     Button btnSignIn;
     LinearLayout btnSignInGoogle, btnSignInFacebook;
 
@@ -32,6 +38,8 @@ public class SignInActivity extends AppCompatActivity {
         btnSignIn = findViewById(R.id.btnSignIn);
         btnSignInFacebook = findViewById(R.id.btnSignInFacebook);
         btnSignInGoogle = findViewById(R.id.btnSignInGoogle);
+        needMail = findViewById(R.id.needMail);
+        needPass = findViewById(R.id.needPass);
     }
 
     @Override
@@ -39,6 +47,7 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
+        getControl();
         FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
@@ -49,8 +58,30 @@ public class SignInActivity extends AppCompatActivity {
             email = String.valueOf(inputEmailSignIn.getText());
             pass = String.valueOf(inputPassSignIn.getText());
             if(TextUtils.isEmpty(email)){
-
+                needMail.setText("Hãy nhập email");
+                return;
             }
+            if(TextUtils.isEmpty(pass)){
+                needPass.setText("Hãy nhập mật khẩu");
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(SignInActivity.this, "Login successful",Toast.LENGTH_LONG).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignInActivity.this, "Authentication failed.",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
         }
     };
 }
--- ReadNewsApp/app/src/main/java/project/an/readnewsapp/SplashAcitvity.java
+++ ReadNewsApp/app/src/main/java/project/an/readnewsapp/SplashAcitvity.java
@@ -3,6 +3,7 @@ package project.an.readnewsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashAcitvity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,17 @@ public class SplashAcitvity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashAcitvity.this, IntroActivity.class);
-                startActivity(intent);
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (currentUser != null) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent intent = new Intent(SplashAcitvity.this, MainActivity.class);
+                    // Người dùng đã đăng nhập, chuyển đến MainActivity
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(SplashAcitvity.this, IntroActivity.class);
+                    // Người dùng chưa đăng nhập, chuyển đến IntroActivity
+                    startActivity(intent);
+                }
                 finish();
             }
         }, 2000);
