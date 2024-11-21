83d44cbc408cc66faa774893038f78200d9e053a
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-21 10:27:25 +0700

    Thêm các fragment

--- ReadNewsApp/app/src/main/java/project/an/readnewsapp/MainActivity.java
+++ ReadNewsApp/app/src/main/java/project/an/readnewsapp/MainActivity.java
@@ -1,20 +1,49 @@
 package project.an.readnewsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView homeNav, profileNav, categoriesNav, bookmarkNav;
+    private void getControl(){
+        homeNav = findViewById(R.id.homeNav);
+        profileNav = findViewById(R.id.profileNav);
+        categoriesNav = findViewById(R.id.categoriesNav);
+        bookmarkNav = findViewById(R.id.bookmarkNav);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        getControl();
+        homeNav.setOnClickListener(homeClick);
+    }
 
+    View.OnClickListener homeClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            replaceFragment(new HomeFragment());
+        }
+    };
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
