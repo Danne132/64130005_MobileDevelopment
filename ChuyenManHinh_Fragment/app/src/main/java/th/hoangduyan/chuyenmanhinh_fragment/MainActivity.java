bd07b8111766640b8550255a8da3ba79a1991666
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-12-03 15:54:55 +0700

    Thêm các Activity và chuyển giao diện

--- ChuyenManHinh_Fragment/app/src/main/java/th/hoangduyan/chuyenmanhinh_fragment/MainActivity.java
+++ ChuyenManHinh_Fragment/app/src/main/java/th/hoangduyan/chuyenmanhinh_fragment/MainActivity.java
@@ -1,6 +1,8 @@
 package th.hoangduyan.chuyenmanhinh_fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -13,11 +15,41 @@ public class MainActivity extends AppCompatActivity {
 
     Button englishBtn, mathBtn, codeBtn;
 
+    private void getControl(){
+        englishBtn = findViewById(R.id.englishBtn);
+        mathBtn = findViewById(R.id.mathBtn);
+        codeBtn = findViewById(R.id.codeBtn);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        englishBtn.setOnClickListener(englishClick);
     }
+
+    View.OnClickListener englishClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, EnglishActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener mathClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, MathActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener codeClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, EnglishActivity.class);
+            startActivity(intent);
+        }
+    };
 }
