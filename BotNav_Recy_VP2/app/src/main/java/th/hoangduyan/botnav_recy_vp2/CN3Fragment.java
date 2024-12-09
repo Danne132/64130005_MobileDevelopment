c0b78cdd56115000ea6e17fc006ce9be5cc5d30f
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-12-09 10:39:36 +0700

    update CN3

--- BotNav_Recy_VP2/app/src/main/java/th/hoangduyan/botnav_recy_vp2/CN3Fragment.java
+++ BotNav_Recy_VP2/app/src/main/java/th/hoangduyan/botnav_recy_vp2/CN3Fragment.java
@@ -3,10 +3,16 @@ package th.hoangduyan.botnav_recy_vp2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +21,49 @@ import android.view.ViewGroup;
  */
 public class CN3Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ViewPager2 viewPager;
+    private Button btn1, btn2, btn3;
 
     public CN3Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CN3Fragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static CN3Fragment newInstance(String param1, String param2) {
         CN3Fragment fragment = new CN3Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_c_n3, container, false);
+        View view = inflater.inflate(R.layout.fragment_c_n3, container, false);
+        viewPager = view.findViewById(R.id.view_pager);
+        btn1 = view.findViewById(R.id.btn1);
+        btn2 = view.findViewById(R.id.btn2);
+        btn3 = view.findViewById(R.id.btn3);
+
+
+        // Danh sách item hiển thị
+        List<String> items = Arrays.asList("Item 1", "Item 2", "Item 3");
+
+        // Gán adapter
+        ViewPageAdapter adapter = new ViewPageAdapter(items);
+        viewPager.setAdapter(adapter);
+
+        // Xử lý sự kiện nút bấm
+        btn1.setOnClickListener(v -> viewPager.setCurrentItem(0));
+        btn2.setOnClickListener(v -> viewPager.setCurrentItem(1));
+        btn3.setOnClickListener(v -> viewPager.setCurrentItem(2));
+
+        return view;
     }
 }
