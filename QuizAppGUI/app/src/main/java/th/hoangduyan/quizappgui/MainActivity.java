d488175f29f7001914f26cd676cdbccc5724dac9
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-12-10 12:13:52 +0700

    Done giao diện Main

--- QuizAppGUI/app/src/main/java/th/hoangduyan/quizappgui/MainActivity.java
+++ QuizAppGUI/app/src/main/java/th/hoangduyan/quizappgui/MainActivity.java
@@ -1,6 +1,9 @@
 package th.hoangduyan.quizappgui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout cppChoose;
+
+    public void getContronl(){
+        cppChoose = findViewById(R.id.cppChoose);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        cppChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CPPActivity.class);
+                startActivity(intent);
+            }
         });
     }
+
 }
