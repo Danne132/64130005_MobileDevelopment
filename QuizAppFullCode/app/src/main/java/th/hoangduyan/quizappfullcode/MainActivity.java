9ed75be5fa521887e442be2899758a971def304b
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-12-10 13:42:15 +0700

    Thêm câu hỏi

--- QuizAppFullCode/app/src/main/java/th/hoangduyan/quizappfullcode/MainActivity.java
+++ QuizAppFullCode/app/src/main/java/th/hoangduyan/quizappfullcode/MainActivity.java
@@ -1,6 +1,9 @@
 package th.hoangduyan.quizappfullcode;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout cppChoose, javaChoose, pythonChoose;
+
+    public void getControl(){
+        cppChoose = findViewById(R.id.cppChoose);
+        javaChoose = findViewById(R.id.javaChoose);
+        pythonChoose = findViewById(R.id.pythonChoose);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        getControl();
+        cppChoose.setOnClickListener(cppClick);
+        javaChoose.setOnClickListener(javaClick);
+        pythonChoose.setOnClickListener(pythonClick);
     }
+
+    View.OnClickListener cppClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+            intent.putExtra("logo", R.drawable.cpp);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener javaClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+            intent.putExtra("logo", R.drawable.java);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener pythonClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+            intent.putExtra("logo", R.drawable.python);
+            startActivity(intent);
+        }
+    };
 }
