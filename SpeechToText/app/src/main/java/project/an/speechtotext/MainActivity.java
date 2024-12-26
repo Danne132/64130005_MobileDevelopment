2fceb851351b0f84a973c65b34f198122c39c457
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-12-26 20:41:46 +0700

    update code speech to text

--- SpeechToText/app/src/main/java/project/an/speechtotext/MainActivity.java
+++ SpeechToText/app/src/main/java/project/an/speechtotext/MainActivity.java
@@ -13,12 +13,14 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -32,122 +34,38 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SpeechRecognizer speechRecognizer;
-    private TextView txtSpeechResult;
-    private ImageView imageView;
-
-    public static final Integer RecordAudioRequestCode = 1;
-    AlertDialog.Builder alertSpeechDialog;
-    AlertDialog alertDialog;
+    EditText editText;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        txtSpeechResult = findViewById(R.id.txtSpeechResult);
+        editText = findViewById(R.id.txtSpeechResult);
         imageView = findViewById(R.id.imageView);
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED){
-            checkPermission();
-        }
-
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-
-        final Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onReadyForSpeech(Bundle params) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-                ViewGroup viewGroup = findViewById(android.R.id.content);
-                View dialogView = LayoutInflater.from(MainActivity.this).inflate(R.layout.alert_custom, viewGroup, false);
-
-                alertSpeechDialog = new AlertDialog.Builder(MainActivity.this);
-                alertSpeechDialog.setMessage("Listening...");
-                alertSpeechDialog.setView(dialogView);
-                alertDialog = alertSpeechDialog.create();
-                alertDialog.show();
-            }
-
-            @Override
-            public void onRmsChanged(float rmsdB) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] buffer) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int error) {
-
-            }
-
-            @Override
-            public void onResults(Bundle results) {
-                imageView.setImageResource(R.drawable.baseline_mic_24);
-                ArrayList<String> arrayList = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                txtSpeechResult.setText(arrayList.get(0));
-                alertDialog.dismiss();
-            }
-
-            @Override
-            public void onPartialResults(Bundle partialResults) {
-
-            }
-
-            @Override
-            public void onEvent(int eventType, Bundle params) {
-
-            }
-        });
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == event.ACTION_UP){
-                    speechRecognizer.stopListening();
-                }
-                if(event.getAction() == event.ACTION_DOWN){
-                    imageView.setImageResource(R.drawable.baseline_mic_24);
-                    speechRecognizer.startListening(speechIntent);
-                }
-                return false;
+            public void onClick(View v) {
+                speakNow(v);
             }
         });
     }
 
-    private void checkPermission() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                    Manifest.permission.RECORD_AUDIO
-            }, RecordAudioRequestCode);
-        }
+    private void speakNow(View view){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 5);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "start listening...");
+        startActivityForResult(intent, 111);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        speechRecognizer.destroy();
-    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == RecordAudioRequestCode && grantResults.length > 0){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
-            }
+        if(requestCode == 111 && resultCode == RESULT_OK){
+            editText.setText(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
         }
     }
 }
